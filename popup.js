1d0cb65577686122e2fac5454e27970baa01a068
Avery Lemons <32022420+Lemonsavery@users.noreply.github.com>
2022-01-26 15:26:51 -0600

    Implement functionality for the openToolNewWindow setting: "Open this tool in new window." (#12)
    Also tweak the setting description text.

--- popup.js
+++ popup.js
@@ -1,2 +1,7 @@
-chrome.tabs.create({url: "page.html"});
+if (localStorage.getItem("openToolNewWindow") === "true") {
+    chrome.windows.create({url: "page.html"})
+} else {
+    chrome.tabs.create({url: "page.html"});
+} // Implementation of openToolNewWindow setting.
+
 window.close();
